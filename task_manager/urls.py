2fb0ae83866d71f6ddfacad77ba23c30179f2098
marentsov <mr.marentsov@gmail.com>
2025-04-09 09:15:59 +0300

    add index template

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from task_manager import views
+from task_manager.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', IndexView.as_view(), name='index')
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
 
-def index(request):
-    return render(
-        request,
-    'index.html',
-        context={
-            'title': 'главная Task-manager',
-            'hello': 'Привет от Task-manager',
-        }
-    )
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'главная Task manager'
+        return context
+
+# def index(request):
+#     return render(
+#         request,
+#     'index.html',
+#         context={
+#             'title': 'главная Task-manager',
+#             'hello': 'Привет от Task-manager',
+#         }
+#     )
