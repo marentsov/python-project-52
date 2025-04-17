a5d93a10b420654db0b91462b30ac42488d8ec63
marentsov <mr.marentsov@gmail.com>
2025-04-17 18:51:29 +0300

    update language

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -129,7 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = os.getenv('LANGUAGE_CODE')
+LANGUAGE_CODE = 'ru'
 
 LANGUAGES = (
     ('en', 'English'),
