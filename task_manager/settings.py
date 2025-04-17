3ff4bc0017f963bd7c8bca0cb991d3e1e163a8b5
marentsov <mr.marentsov@gmail.com>
2025-04-17 14:25:16 +0300

    update settingd with static

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -153,8 +153,8 @@ LOCALE_PATHS = [
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media files
 MEDIA_URL = '/media/'
