84fdb736765fb3e78f27de453e075459f03cc198
marentsov <mr.marentsov@gmail.com>
2025-04-21 15:17:57 +0300

    update CI

--- task_manager/settings_test.py
+++ task_manager/settings_test.py
@@ -3,7 +3,7 @@ from .settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'task_manager/db.sqlite3',
     }
 }
 
